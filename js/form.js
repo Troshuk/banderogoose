3aeb8bc48a5b093ca651a478941db282ea8e9a26
Denys Troshuk <Troshuk@gmail.com>
2023-06-27 13:28:32 -0400

    fix js, add comments

--- js/form.js
+++ js/form.js
@@ -2,12 +2,15 @@ const form = document.querySelector('#form');
 const launchBtn = document.querySelector('#launch-btn');
 const goToFormButton = document.querySelector('#go-to-form-btn');
 const userEmailField = document.querySelector('#user-email');
+const animationTime = 4000;
+const gooseAminationPath = './img/gus-anim.gif';
+const gooseAnimationClass = 'gus-anim';
 
 
-
+// Smoothly scroll to the form
 goToFormButton.addEventListener('click', function (e) {
     e.preventDefault();
-    form.scrollIntoView();
+    form.scrollIntoView({behavior: "smooth"});
 });
 
 function clearFormFields() {
@@ -18,23 +21,23 @@ function clearFormFields() {
     fieldEmail.value = '';
 }
 
+// Create a new tag for animation
 function addGooseElement() {
-    const targetContainer = document.querySelector('#form');
     const gooseEl = document.createElement('img');
-    gooseEl.classList.add('gus-anim');
+    gooseEl.classList.add(gooseAnimationClass);
 
-    targetContainer.appendChild(gooseEl);
+    form.appendChild(gooseEl);
 }
 
 function showGooseAnim() {
-    const gooseEl = document.querySelector('.gus-anim');
+    const gooseEl = document.querySelector('.' + gooseAnimationClass);
 
-    gooseEl.setAttribute('src', './img/gus-anim.gif');
+    gooseEl.setAttribute('src', gooseAminationPath);
     
-
+    // Set animation time
     setTimeout(() => {
         gooseEl.removeAttribute('src');
-    }, 4000)
+    }, animationTime)
 }
 
 addGooseElement();
@@ -43,24 +46,27 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
 
+    // Disable button (prevent double clicking)
     launchBtn.setAttribute('disabled', true);
     launchBtn.style.opacity = '0.7';
 
+    // Send API request to `netlify`
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
+      // After request has been sent
       .then(() => {
         showGooseAnim();
 
-        launchBtn.removeAttribute('disabled')
-        clearFormFields();
-
-
+        // Wait time for the animation to get complete
         setTimeout(() => {
+            // Enable button again for the next form submition
+            launchBtn.removeAttribute('disabled')
+            clearFormFields();
             launchBtn.style.opacity = '1';
-        }, 4000)
+        }, animationTime)
       })
       .catch((error) => console.log('Sending form failed'));
 })
